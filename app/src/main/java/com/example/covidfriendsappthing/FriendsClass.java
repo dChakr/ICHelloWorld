33e5fffffbc02e26072c7a63b36dec451e0ad022
ak1920 <ak1920@ic.ac.uk>
2021-03-27 22:29:13 +0000

    friends list is clickable

--- app/src/main/java/com/example/covidfriendsappthing/FriendsClass.java
+++ app/src/main/java/com/example/covidfriendsappthing/FriendsClass.java
@@ -2,8 +2,7 @@ package com.example.covidfriendsappthing;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
+
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
