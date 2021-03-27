171e19a3b1b925ce6b76eccb9ff19e5767cb244c
Viktoria Adorjan <viktoria.adorjan20@imperial.ac.uk>
2021-03-27 19:41:35 +0000

    registerButton onCreate method

--- app/src/main/java/com/example/covidfriendsappthing/RegisterClass.java
+++ app/src/main/java/com/example/covidfriendsappthing/RegisterClass.java
@@ -1,6 +1,8 @@
 package com.example.covidfriendsappthing;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,6 +10,20 @@ public class RegisterClass extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Button registerButton = findViewById(R.id.settings_button);
+        registerButton.setOnClickListener(x -> {
+            EditText regUsernameField = findViewById(R.id.reg_username);
+            String regUsername = regUsernameField.getText().toString();
+
+            EditText regPasswordField = findViewById(R.id.user_password);
+            String regPassword = regPasswordField.getText().toString();
+
+            EditText regEmailField = findViewById(R.id.user_email);
+            String regEmail = regEmailField.getText().toString();
+
+            EditText phoneNumField = findViewById(R.id.phone_number);
+            String phoneNum = phoneNumField.getText().toString();
+        });
         setContentView(R.layout.register_page);
     }
 }
