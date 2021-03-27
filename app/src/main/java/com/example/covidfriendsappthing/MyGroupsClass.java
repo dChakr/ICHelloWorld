86ed2dadc9e410e8293c866a866049d9142afc4e
ak1920 <ak1920@ic.ac.uk>
2021-03-27 15:57:04 +0000

    main screen done ish - take 2

---
+++ app/src/main/java/com/example/covidfriendsappthing/MyGroupsClass.java
@@ -0,0 +1,13 @@
+package com.example.covidfriendsappthing;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MyGroupsClass extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.my_groups);
+    }
+}
