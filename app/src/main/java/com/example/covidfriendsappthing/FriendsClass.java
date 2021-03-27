97d968f9b7dada0ba492fb145ea9158175c51ca2
Viktoria Adorjan <viktoria.adorjan20@imperial.ac.uk>
2021-03-27 21:16:23 +0000

    FriendClass doesn't work

--- app/src/main/java/com/example/covidfriendsappthing/FriendsClass.java
+++ app/src/main/java/com/example/covidfriendsappthing/FriendsClass.java
@@ -1,13 +1,30 @@
 package com.example.covidfriendsappthing;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FriendsClass extends AppCompatActivity {
+
+    List<String> friends = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.friends_list);
+
+        friends.add("this");
+        friends.add("that");
+
+        ArrayAdapter<String> arrayAdapter
+                = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, friends);
+
+        ListView listView = findViewById(R.id.friends_list);
+        listView.setAdapter(arrayAdapter);
     }
 }
