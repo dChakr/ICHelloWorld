488ce12c5f9d04e8a86fb35ec7b6143c4cbda4a6
Dyuti Chakraborty <dc420@imperial.ac.uk>
2021-03-28 11:59:33 +0100

    demo meeting thing

--- app/src/main/java/com/example/covidfriendsappthing/MyGroupsClass.java
+++ app/src/main/java/com/example/covidfriendsappthing/MyGroupsClass.java
@@ -1,6 +1,11 @@
 package com.example.covidfriendsappthing;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,6 +15,26 @@ public class MyGroupsClass extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.my_groups);
 
+        Button viewMembers = (Button) findViewById(R.id.view_members1);
+
+        viewMembers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView meetingNameBox = findViewById(R.id.group_name1);
+                String meetingName = meetingNameBox.getText().toString();
+
+                Intent meeting = new Intent(MyGroupsClass.this, CreatedMeetingActivity2.class);
+
+                meeting.putExtra("meetingName", meetingName);
+                meeting.putExtra("member1", "Mum");
+                meeting.putExtra("member2", "Dad");
+                meeting.putExtra("member3", "Daniel");
+                meeting.putExtra("member4", "Alice");
+                meeting.putExtra("member5", "Tom");
+
+                startActivity(meeting);
+            }
+        });
 
     }
 }
