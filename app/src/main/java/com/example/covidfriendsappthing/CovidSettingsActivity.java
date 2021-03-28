8c00efdb647b9ccea3cedbdaafaebc16ad024c34
Viktoria Adorjan <viktoria.adorjan20@imperial.ac.uk>
2021-03-28 00:36:46 +0000

    CovidSettings radiobuttons

--- app/src/main/java/com/example/covidfriendsappthing/CovidSettingsActivity.java
+++ app/src/main/java/com/example/covidfriendsappthing/CovidSettingsActivity.java
@@ -1,10 +1,12 @@
 package com.example.covidfriendsappthing;
 
 import android.os.Bundle;
+import android.widget.CheckBox;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CovidSettingsActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
