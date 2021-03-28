0fc79c8a8cb90a278ff734da7db5b74c56479b54
Ultra <Athaarukanta@gmail.com>
2021-03-28 10:30:41 +0100

    addfirends is now possible

--- app/src/main/java/com/example/covidfriendsappthing/AddFriend.java
+++ app/src/main/java/com/example/covidfriendsappthing/AddFriend.java
@@ -7,6 +7,12 @@ import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class AddFriend extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +25,27 @@ public class AddFriend extends AppCompatActivity {
         addFriendButton.setOnClickListener(x -> {
             String toAdd = addFriendField.getText().toString();
 
+            FirebaseAuth auth = FirebaseAuth.getInstance();
+            DatabaseReference database = FirebaseDatabase.getInstance("https://pandemicpals-f29e4-default-rtdb.europe-west1.firebasedatabase.app/").getReference();
 
+            String uid  = auth.getUid();
+            database.child("Users").child(uid).get().addOnSuccessListener(dataSnapshot -> {
+                User user = dataSnapshot.getValue(User.class);
+
+                database.child("Users").get().addOnSuccessListener(dataSnapshot1 -> {
+                    for (DataSnapshot ds: dataSnapshot1.getChildren()) {
+                        User user1  = ds.getValue(User.class);
+                        if(user1.username.equals(toAdd)){
+                            user1.friends.add(uid);
+                            user.friends.add(ds.getKey());
+                            database.child("Users").child(uid).child("friends").setValue(user.friends);
+                            database.child("Users").child(ds.getKey()).child("friends").setValue(user.friends);
+
+                        }
+                    }
+                });
+
+            });
 
 
             Intent myGroupsIntent = new Intent(AddFriend.this, MainActivity.class);
